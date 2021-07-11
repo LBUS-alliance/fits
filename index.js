9f388ed79f7520233e89429d9d85325c15e62034
Mikayla <mikayla61220@gmail.com>
2021-07-10 23:07:01 -0700

    add ship imgs

--- index.js
+++ index.js
@@ -13,14 +13,24 @@ var fits = {};
     let fits_list = '';
     let modals = '';
 
-    for (const fit in fits) {
-      let fit_text = fits[fit];
+    for (const fitId in fits) {
+      const fitData = fits[fitId];
+      let fit_text = fitData.fit;
 
-      fits_list += `
-        <a class="collection-item modal-trigger fit" id="${fit}" href="#modal-${fit}">
-          ${fit}
-        </a>
-      `;
+      if (fitData.img) {
+        fits_list += `
+          <li class="collection-item modal-trigger fit with-img" id="${fitId}" href="#modal-${fitId}">
+            <img src="${fitData.img}" class="circle"/>
+            <span>${fitId}<span>
+          </li>
+        `;
+      } else {
+        fits_list += `
+          <li class="collection-item modal-trigger fit no-img" id="${fitId}" href="#modal-${fitId}">
+            <span>${fitId}<span>
+          </li>
+        `;
+      }      
 
       let fit_html = '';
       for (const line of fit_text.split("\n")) {
@@ -28,13 +38,13 @@ var fits = {};
       }
 
       modals += `
-        <div id="modal-${fit}" class="modal modal-fixed-footer">
+        <div id="modal-${fitId}" class="modal modal-fixed-footer">
           <div class="modal-content">
-            <h4>${fit}</h4>
+            <h4>${fitId}</h4>
             <p>${fit_html}</p>
           </div>
           <div class="modal-footer">
-            <button class="waves-effect waves-teal btn-flat" id="modal-copy-${fit}" onclick="copyFit('modal-copy-${fit}', \`${fit_text}\`)">Copy</button>
+            <button class="waves-effect waves-teal btn-flat" id="modal-copy-${fitId}" onclick="copyFit('modal-copy-${fitId}', \`${fit_text}\`)">Copy</button>
             <button class="modal-close waves-effect waves-teal btn-flat">Close</button>
           </div>
         </div>
@@ -46,7 +56,7 @@ var fits = {};
     $('#modals').html(modals);
 
     $('.fit').css({ backgroundColor: '#303437', color: 'white' });
-    changeFitColor(fits, 'white', '#26a69a', '#303437', 'white');
+    changeFitColor(fits, '#455a64', 'white', '#303437', 'white');
     $('.modal').css({ backgroundColor: '#303437', color: 'white' });
     $('.modal-footer').css({ backgroundColor: '#303437', color: 'white', borderTop: '1px solid rgba(0,0,0,0.4)' });
     $('.modal-footer button').removeClass('btn-flat').addClass('btn');
@@ -90,10 +100,10 @@ const toggleColorMode = () => {
     if (!fits) {
       $.get('fits.yml', data => {
         fits = jsyaml.load(data);
-        changeFitColor(fits, 'white', '#26a69a', '#303437', 'white');
+        changeFitColor(fits, '#455a64', 'white', '#303437', 'white');
       });
     } else {
-      changeFitColor(fits, 'white', '#26a69a', '#303437', 'white');
+      changeFitColor(fits, '#455a64', 'white', '#303437', 'white');
     }
     $('.modal').css({ backgroundColor: '#303437', color: 'white' });
     $('.modal-footer').css({ backgroundColor: '#303437', color: 'white', borderTop: '1px solid rgba(0,0,0,0.4)' });
